5263fea36f12591c066a529f99ad13d0d84ec5ec
Ryan <cumt.xiaochi@gmail.com>
2012-11-06 16:46:27 +0800

    add title

--- data/sitemap.js
+++ data/sitemap.js
@@ -3,6 +3,6 @@
 (function() {
     var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
     var _creator = function() { return _(b,[_(c,d,e,f,g,h,i,[_(c,j,e,f,g,k),_(c,l,e,f,g,m),_(c,n,e,f,g,o),_(c,p,e,f,g,q)])]);}; 
-var b="rootNodes",c="pageName",d="Home",e="type",f="Wireframe",g="url",h="Home.html",i="children",j="post a prophecy",k="post_a_prophecy.html",l="prophecy-list(index)",m="prophecy_list_index_.html",n="prohecy",o="prohecy.html",p="User",q="User.html";
+var b="rootNodes",c="pageName",d="大家都是预言家：页面流程图",e="type",f="Wireframe",g="url",h="大家都是预言家：页面流程图.html",i="children",j="post a prophecy",k="post_a_prophecy.html",l="prophecy-list(index)",m="prophecy_list_index_.html",n="prohecy",o="prohecy.html",p="User",q="User.html";
 return _creator();
 })();
---
+++ 大家都是预言家：页面流程图_files/axurerp_pagespecificscript.js
@@ -0,0 +1,51 @@
+﻿for(var i = 0; i < 18; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
+
+$axure.eventManager.pageLoad(
+function (e) {
+
+});
+gv_vAlignTable['u3'] = 'center';gv_vAlignTable['u15'] = 'center';document.getElementById('u4_img').tabIndex = 0;
+
+u4.style.cursor = 'pointer';
+$axure.eventManager.click('u4', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('prohecy.html');
+
+}
+});
+document.getElementById('u0_img').tabIndex = 0;
+
+u0.style.cursor = 'pointer';
+$axure.eventManager.click('u0', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('post_a_prophecy.html');
+
+}
+});
+gv_vAlignTable['u17'] = 'center';gv_vAlignTable['u5'] = 'center';gv_vAlignTable['u1'] = 'center';gv_vAlignTable['u9'] = 'center';document.getElementById('u6_img').tabIndex = 0;
+
+u6.style.cursor = 'pointer';
+$axure.eventManager.click('u6', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('User.html');
+
+}
+});
+document.getElementById('u2_img').tabIndex = 0;
+
+u2.style.cursor = 'pointer';
+$axure.eventManager.click('u2', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('prophecy_list_index_.html');
+
+}
+});
+gv_vAlignTable['u11'] = 'center';gv_vAlignTable['u13'] = 'center';gv_vAlignTable['u7'] = 'center';
