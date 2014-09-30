96fdbf0b9213e86899145c6024bed593a176b1d2
picasso250 <cumt.xiaochi@gmail.com>
2014-10-01 02:03:34 +0800

    config db

--- configs/development.php
+++ configs/development.php
@@ -3,5 +3,8 @@
 return [
     'debug' => true,
     'log.writer' => new \Slim\LogWriter(ROOT.'/app.log'),
-    'log.level' => \Slim\Log::DEBUG
+    'log.level' => \Slim\Log::DEBUG,
+    'db.dsn' => 'mysql:host=localhost;dbname=predict',
+    'db.username' => 'root',
+    'db.password' => '',
 ];
--- configs/production.php
+++ configs/production.php
@@ -3,5 +3,8 @@
 return [
     'debug' => false,
     'log.writer' => new \Slim\LogWriter('/home/bae/log'),
-    'log.level' => \Slim\Log::WARN
+    'log.level' => \Slim\Log::WARN,
+    'db.dsn' => 'mysql:host=sqld.duapp.com;port=4050;dbname=IQAPFqdRdzPKLaSlvYYC',
+    'db.username' => '1qlGvUtuDM9y8pVOF2Yfs54u',
+    'db.password' => '9wAZ7tXu0uzRbTjTjWOlPM00hStCIpAp',
 ];
