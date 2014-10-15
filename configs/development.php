2963f407f77c39506041bd87b6e1a35cab06daef
wangxiaochi <wangxiaochi@baidu.com>
2014-10-15 16:16:12 +0800

    config

--- configs/development.php
+++ configs/development.php
@@ -1,5 +1,8 @@
 <?php
 
+$cache = new Memcache();
+$cache->connect('nmg01-ssl-gxt-test-meta.nmg01.baidu.com', 8201);
+
 return [
     'debug' => true,
     'log.path' => ROOT.'/app.log',
@@ -7,4 +10,5 @@ return [
     'db.dsn' => 'mysql:host=localhost;dbname=predict',
     'db.username' => 'root',
     'db.password' => '',
+    'cache' => $cache,
 ];
--- configs/production.php
+++ configs/production.php
@@ -7,4 +7,5 @@ return [
     'db.dsn' => 'mysql:host=sqld.duapp.com;port=4050;dbname=IQAPFqdRdzPKLaSlvYYC',
     'db.username' => '1qlGvUtuDM9y8pVOF2Yfs54u',
     'db.password' => '9wAZ7tXu0uzRbTjTjWOlPM00hStCIpAp',
+    'cache' => $cache,
 ];
